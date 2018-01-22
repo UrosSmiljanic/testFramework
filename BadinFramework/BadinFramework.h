be18300080e2f23a00b91ca60d74489073518e26
Uros Smiljanic <smiljanic.uros@gmail.com>
2018-01-22 14:51:11 +0100

    Initial Commit

---
+++ BadinFramework/BadinFramework.h
@@ -0,0 +1,19 @@
+//
+//  BadinFramework.h
+//  BadinFramework
+//
+//  Created by Uros Smiljanic on 1/22/18.
+//  Copyright © 2018 PanaceaTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BadinFramework.
+FOUNDATION_EXPORT double BadinFrameworkVersionNumber;
+
+//! Project version string for BadinFramework.
+FOUNDATION_EXPORT const unsigned char BadinFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BadinFramework/PublicHeader.h>
+
+
